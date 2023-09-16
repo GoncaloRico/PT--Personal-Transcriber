912ec1a0ea1ba568d4a8251c86a5038f5e237b9a
GoncaloRico <gmkkrico@gmail.com>
2023-09-16 10:18:36 +0100

    Uploading project into Git

---
+++ test_project.py
@@ -0,0 +1,18 @@
+import pytest
+from project import save_recording, record, recording_to_text
+
+def test_record():
+    with pytest.raises(TypeError):
+        record("cat")
+
+def test_missing_voice_file():
+    with pytest.raises(TypeError):
+        recording_to_text()
+
+def test_save_recording():
+    with pytest.raises(SystemExit):
+        save_recording("n", 48000, "")
+    with pytest.raises(SystemExit):
+        save_recording("NO", 48000, "")
+    with pytest.raises(SystemExit):
+        save_recording("no", 48000, "")
