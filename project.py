c05989d5ad826b958f483d6b220c14a07d771399
GoncaloRico <gmkkrico@gmail.com>
2023-09-17 11:02:55 +0100

    Added option to input custom name for file(s)

--- project.py
+++ project.py
@@ -8,13 +8,14 @@ import sys
 def main(): #This function will record an audio as soon as the program runs, until a key is pressed, interrupting the recording, and then play it back to the user.
     duration = collect_user_input()
     save_or_not, SAMPLE_RATE, audio_recording = record(duration)
-    voice_file_name = save_recording(save_or_not)
-    text_file_name = voice_file_name.replace(".wav", ".txt")
+    voice_file_name = save_recording(save_or_not, SAMPLE_RATE, audio_recording)
     text_from_voice = recording_to_text(voice_file_name)
+    text_file_name = voice_file_name.replace(".wav", ".txt")
     save_text_file(text_from_voice, text_file_name)
 
 
 def collect_user_input():
+    user_input = ""
     while not isinstance(user_input, int): #This will loop the request for user input on the amount of seconds the recording should be, until an integer is submitted
         try:
             user_input = int(input("How long do you want this recording to be? (Please insert duration in seconds) "))
@@ -41,7 +42,11 @@ def record(duration):
 
 def save_recording(save_or_not, SAMPLE_RATE, audio_recording): #This processes whether the voice file will be saved or discarded
     if save_or_not in ["yes", "y"]:
-        voice_file_name = time.strftime("%d_%m_%Y-%H_%M_%S.wav")
+        custom_voice_file_name = input("Do you want to use a custom name for the file? Please select yes or no. ") #Option for user to insert their own name, instead of date and time as file name(s)
+        if custom_voice_file_name in ["yes", "y"]:
+            voice_file_name = (input("Please insert a name for the voice file: ")) + ".wav"
+        else:
+            voice_file_name = time.strftime("%d_%m_%Y-%H_%M_%S.wav")
         wav.write(voice_file_name, SAMPLE_RATE, audio_recording)
         return(voice_file_name)
     else:
@@ -52,14 +57,15 @@ def recording_to_text(voice_file_name):
     r = sr.Recognizer()
     with sr.AudioFile(voice_file_name) as source: #This will open the file
         audio_data = r.record(source) #load audio to memory
-        text = r.recognize_google(audio_data) #convert from speech to text
+        text = r.recognize_google(audio_data, language = "en-US", key=None, show_all=True) #convert from speech to text
         return text
 
 
 def save_text_file(text_from_voice, text_file_name):
     with open(text_file_name, "w") as f:
-        f.write(text_from_voice)
+        f.write(str(text_from_voice))
     print("Your Audio Recording has been turned to text.")
+
 
 if __name__ == "__main__":
     main()
